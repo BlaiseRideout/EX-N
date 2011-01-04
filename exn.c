425a397c222d7bae25331f2caacb786c470161f4
LAMMJohnson <john_anthony@lavabit.com>
2011-01-04 08:19:39 +0000

    first commit

---
+++ exn.c
@@ -0,0 +1,366 @@
+/* EXN is written by John Anthony <john_anthony@lavabit.com>, 2010.
+ *
+ * This software is in the public domain
+ * and is provided AS IS, with NO WARRANTY.
+ * 
+ * Extra special thanks go to the dwm engineers for inspiration and
+ * both the dwm engineers (from whom I stole a fair few chunks of code)
+ * and Nick Welch (author of TinyWM) for providing such great learning
+ * material -- without whom I would probably still be looking through
+ * obscure documentation. */
+
+#include <stdlib.h>
+#include <X11/Xlib.h>
+#include <X11/cursorfont.h>
+#include <X11/keysym.h>
+#include <unistd.h>
+#ifdef XINERAMA
+#include <X11/extensions/Xinerama.h>
+#endif
+
+#include <stdio.h>
+
+/* Macros */
+#define CLEANMASK(mask)         (mask & ~(numlockmask|LockMask))
+#define LENGTH(X)       (sizeof X / sizeof X[0])
+#define MAX(a, b)       ((a) > (b) ? (a) : (b))
+
+#define NUMMONS         2
+
+/* Datatypes */
+typedef union {
+    int i;
+    unsigned int ui;
+    float f;
+    const void *v;
+} Arg; 
+
+typedef struct Client Client;
+struct Client{
+    Window win;
+    Client *prev, *next;
+};
+
+typedef struct{
+    int x, y, w, h;
+    Client *first, *last, *current;
+} Monitor;
+
+typedef struct {
+    unsigned int mod;
+    KeySym keysym;
+    void (*func)(const Arg *);
+    const Arg arg;
+} Key;
+
+/* Predecs */
+static void attachwindow(Monitor *m, Window w);
+static void configurerequest(XEvent *e);
+static void cyclewin(const Arg *arg);
+static void destroynotify(XEvent *e);
+static void detachwindow(Monitor *m, Window w);
+static void focusin(XEvent *e);
+static void grabkeys(void);
+static void initmons(void);
+static void keypress(XEvent *e);
+static void killclient(const Arg *arg);
+static void refocus(void);
+static void quit(const Arg *arg);
+static void mapnotify(XEvent *e);
+static void monfoc(const Arg *arg);
+static void monmove(const Arg *arg);
+static void updatenumlockmask(void);
+
+/* Variables */
+static unsigned int numlockmask = 0;
+static int selmon = 1;
+static void (*handler[LASTEvent]) (XEvent *) = {
+    /* [ButtonPress] = buttonpress, */
+    /* [ClientMessage] = clientmessage, */
+    [ConfigureRequest] = configurerequest,
+    /* [ConfigureNotify] = configurenotify, */
+    [DestroyNotify] = destroynotify,
+    /* [EnterNotify] = enternotify, */
+    /* [Expose] = expose, */
+    [FocusIn] = focusin,
+    [KeyPress] = keypress,
+    [MappingNotify] = mapnotify,
+    /* [MapRequest] = maprequest, */
+    /* [PropertyNotify] = propertynotify, */
+    /* [UnmapNotify] = unmapnotify */
+};
+static Bool running = True;
+static Display *dpy;
+static Monitor mons[NUMMONS];
+static Window root;
+
+/* Config file goes here */
+#define MODKEY Mod4Mask
+
+static Key keys[] = {
+    {MODKEY,            XK_Left,        monfoc,     {.i = -1}   },
+    {MODKEY|ShiftMask,  XK_Left,        monmove,    {.i = -1}   },
+    {MODKEY,            XK_Right,       monfoc,     {.i = 1}    },
+    {MODKEY|ShiftMask,  XK_Right,       monmove,    {.i = 1}    },
+    {Mod1Mask,          XK_Tab,         cyclewin,   {.i = 1}    },
+    {Mod1Mask|ShiftMask,XK_Tab,         cyclewin,   {.i = -1},  },
+    {MODKEY|ShiftMask,  XK_q,           quit,       {0}         },
+    {Mod1Mask,          XK_F4,          killclient, {0}         },
+};
+/* End of config file stuffs */
+
+void
+attachwindow(Monitor* m, Window w) {
+
+    Client *c, *find;
+
+    c = (Client *)calloc(1, sizeof(Client));
+    c->win = w;
+    c->next = NULL;
+
+    if (!m->first) {
+        m->first = c;
+        c->prev = NULL;
+    }
+    else {
+        for (find = m->first; find->next; find = find->next);
+        find->next = c;
+        c->prev = find;
+    }
+
+    m->last = c;
+    m->current = c;
+}
+
+void
+configurerequest(XEvent *e) {
+
+}
+
+void
+cyclewin(const Arg *arg) {
+
+    if (arg->i > 0) {
+        if(mons[selmon].current->next)
+            mons[selmon].current = mons[selmon].current->next;
+        else
+            mons[selmon].current = mons[selmon].first;
+    }
+    else {
+        if(mons[selmon].current->prev)
+            mons[selmon].current = mons[selmon].current->prev;
+        else
+            mons[selmon].current = mons[selmon].last;
+    }
+   
+    refocus();
+}
+
+void
+destroynotify(XEvent *e) {
+    XDestroyWindowEvent *ev = &e->xdestroywindow;
+    detachwindow(&mons[selmon], ev->window);
+
+    //Logic for removing old client (dealloc)
+    //
+    //
+
+    refocus();
+}
+
+void
+detachwindow(Monitor* m, Window w) {
+    Client *tcli;
+
+    for (tcli = m->first; tcli->win != w && tcli != NULL; tcli = tcli->next);
+
+    if (!tcli)
+        return;
+
+    if (tcli->next)
+        tcli->next->prev = tcli->prev;
+    else                                /* Deal with the eventuality that it could be the last in the list */
+        m->last = tcli->prev;
+
+    if (tcli->prev) {
+        m->current = tcli->prev;
+        tcli->prev->next = tcli->next;
+    }
+    else {                               /* Deal with the eventuality of it being the first in the list */
+        m->first = tcli->next;
+        m->current = m->first;
+    }
+}
+
+void
+focusin(XEvent *e) { /* there are some broken focus acquiring clients */
+
+}
+
+void
+grabkeys(void) {
+    updatenumlockmask();
+        unsigned int i, j;
+        unsigned int modifiers[] = { 0, LockMask, numlockmask, numlockmask|LockMask };
+        KeyCode code;
+
+        XUngrabKey(dpy, AnyKey, AnyModifier, root);
+        for(i = 0; i < LENGTH(keys); i++) {
+            if((code = XKeysymToKeycode(dpy, keys[i].keysym)))
+                for(j = 0; j < LENGTH(modifiers); j++)
+                    XGrabKey(dpy, code, keys[i].mod | modifiers[j], root,
+                            True, GrabModeAsync, GrabModeAsync);
+        }
+}
+
+void
+initmons(void) {    /* Needs to be generalised using Xinerama */
+
+    mons[0].x = 0;
+    mons[0].y = 0;
+    mons[0].w = 1280;
+    mons[0].h = 1024;
+    mons[1].x = 1280;
+    mons[1].y = 0;
+    mons[1].w = 1920;
+    mons[1].h = 1080;
+
+    mons[0].first = NULL;
+    mons[0].last = NULL;
+    mons[0].current = NULL;
+
+    mons[1].first = NULL;
+    mons[1].last = NULL;
+    mons[1].current = NULL;
+}
+
+void
+keypress(XEvent *e) {
+    unsigned int i;
+    KeySym keysym;
+    XKeyEvent *ev; 
+
+    ev = &e->xkey;
+    keysym = XKeycodeToKeysym(dpy, (KeyCode)ev->keycode, 0);
+    for(i = 0; i < LENGTH(keys); i++) 
+        if(keysym == keys[i].keysym
+                && CLEANMASK(keys[i].mod) == CLEANMASK(ev->state)
+                && keys[i].func)
+            keys[i].func(&(keys[i].arg));
+
+}
+
+void
+killclient(const Arg *arg) {
+
+    if (!mons[selmon].current)
+        return;
+
+    /* OUT OF ACTION until window atoms are set up correctly! */
+
+    /* Window w = mons[selmon].current->win; */
+
+    /* detachwindow(&mons[selmon], w); */
+    /* refocus(); */
+    /* XKillClient(dpy, w); */
+}
+
+void
+mapnotify(XEvent *e) {
+    Window w = e->xmap.window;
+    attachwindow(&mons[selmon], w);
+    XMoveResizeWindow(dpy, w, mons[selmon].x, mons[selmon].y, mons[selmon].w, mons[selmon].h);
+    refocus();
+}
+
+void
+monmove(const Arg *arg) {
+
+    Window w = mons[selmon].current->win;
+    Monitor *new, *old = &mons[selmon];
+    monfoc(arg);
+    new = &mons[selmon];
+    if (new != old) {
+        detachwindow(old, w);
+        attachwindow(new, w);
+        XMoveResizeWindow(dpy, w, mons[selmon].x, mons[selmon].y, mons[selmon].w, mons[selmon].h);
+        refocus();
+    }
+}
+
+void
+monfoc(const Arg *arg) {
+    int n = selmon + arg->i;
+    if (n >= 0 && n < NUMMONS) {
+        selmon = n;
+        if (mons[selmon].current){
+            printf("Setting focus to monitor %i.\n", selmon);
+            refocus();
+        }
+    }
+}
+
+void
+refocus(void) {
+    if (mons[selmon].current) {
+        XSetInputFocus(dpy, mons[selmon].current->win, RevertToPointerRoot, CurrentTime);
+        XRaiseWindow(dpy, mons[selmon].current->win);
+    }
+}
+
+void
+quit(const Arg *arg) {
+    running = False;
+}
+
+void
+updatenumlockmask(void) {
+    unsigned int i, j;
+    XModifierKeymap *modmap;
+
+    numlockmask = 0;
+    modmap = XGetModifierMapping(dpy);
+    for(i = 0; i < 8; i++)
+        for(j = 0; j < modmap->max_keypermod; j++)
+            if(modmap->modifiermap[i * modmap->max_keypermod + j] == XKeysymToKeycode(dpy, XK_Num_Lock))
+                numlockmask = (1 << i);
+    XFreeModifiermap(modmap);
+}
+
+int main()
+{
+    XEvent ev;
+    XSetWindowAttributes wa;
+
+    if(!(dpy = XOpenDisplay(0x0))) return 1;
+    root = DefaultRootWindow(dpy);
+
+    initmons();
+
+    wa.cursor = XCreateFontCursor(dpy, XC_left_ptr);
+    /* wa.event_mask = SubstructureRedirectMask|SubstructureNotifyMask|ButtonPressMask|EnterWindowMask|LeaveWindowMask|StructureNotifyMask|PropertyChangeMask; */
+    wa.event_mask = SubstructureNotifyMask|ButtonPressMask|EnterWindowMask|LeaveWindowMask|StructureNotifyMask|PropertyChangeMask;
+    XChangeWindowAttributes(dpy, root, CWEventMask|CWCursor, &wa);
+    XSelectInput(dpy, root, wa.event_mask);
+    grabkeys();
+    /* XUngrabKey(dpy, AnyKey, AnyModifier, root); */
+
+    while (running) {
+
+        XNextEvent(dpy, &ev);
+        if(ev.type == MapNotify)
+            mapnotify(&ev);
+        else if(ev.type == KeyPress)
+            keypress(&ev);
+        else if(ev.type == DestroyNotify)
+            destroynotify(&ev);
+
+        /* main event loop */
+        /* XSync(dpy, False); */
+        /* while(running && !XNextEvent(dpy, &ev)) { */
+        /*     if(handler[ev.type]) */
+        /*         handler[ev.type](&ev); */
+        /* } */
+
+    }
+}
