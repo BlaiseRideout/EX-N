15898afe64e9a0da8cd9ea6af576be1ceb438b8a
John Anthony <johnanthony@lavabit.com>
2011-09-21 17:34:41 +0100

    Basic data structures done

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 CC=gcc
 FLAGS=--std=c99 -Wall --pedantic
-LIBS=
+LIBS=-lX11 -lXinerama
 
-all:
+all: exn.c config.h
 	${CC} -o exn exn.c ${FLAGS} ${LIBS}
+
+install:
+	cp exn /usr/bin/exn
--- exn.c
+++ exn.c
@@ -4,7 +4,103 @@
  * and is provided AS IS, with NO WARRANTY.
  */
 
-int main(int args, char** argv) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/Xlib.h>
+#include <X11/extensions/Xinerama.h>
 
+#define LENGTH(x)   (sizeof(x) / sizeof(x[0])
+
+typedef struct Client Client;
+struct Client {
+    Window win;
+    Client *next;
+};
+
+typedef struct Monitor Monitor;
+struct Monitor {
+    Client *heads;
+    int curr;
+    Monitor *next;
+    int x;
+    int y;
+    int width;
+    int height;
+};
+
+static void clear_up(void);
+static Monitor* create_mon(int x, int y, int w, int h);
+static void errout(char *msg);
+static void init(void);
+static void* safe_malloc(unsigned int sz, char *errmsg);
+
+static Display *dpy;
+static int screen;
+static Window root;
+static Monitor *MonLL = NULL;
+
+#include "config.h"
+
+static void
+clear_up(void) {
+    XCloseDisplay(dpy);
+}
+
+static Monitor*
+create_mon(int x, int y, int w, int h) {
+    Monitor *m;
+
+    m = safe_malloc(sizeof(Monitor), "Error allocating space for new monitor.");
+    m->x = x;
+    m->y = y;
+    m->width = w;
+    m->height = h;
+
+    return m;
+}
+
+static void
+errout(char* msg) {
+    if (msg)
+        puts(msg);
+    exit(1);
+}
+
+static void
+init(void) {
+    XineramaScreenInfo *info;
+    int i, numscreens;
+    Monitor *m;
+
+    dpy = XOpenDisplay(NULL);
+    if (!dpy)
+        errout("EX^N: Unable to open X display!");
+
+    screen = DefaultScreen(dpy);
+    root = RootWindow(dpy, screen);
+
+    /* Handle Xinerama screen loading */
+    info = XineramaQueryScreens(dpy, &numscreens);
+    for (i = 0; i < numscreens; ++i) {
+        m = MonLL;
+        MonLL = create_mon(info[i].x_org, info[i].y_org, info[i].width, info[i].height);
+        MonLL->next = m;
+    }
+    XFree(info);
+}
+
+static void*
+safe_malloc(unsigned int sz, char *errmsg) {
+    void *mem = malloc(sz);
+    if (!mem)
+        errout(errmsg);
+    return mem;
+}
+
+int main(int argc, char** argv) {
+    init();
+
+
+    clear_up();
     return 0;
 }
